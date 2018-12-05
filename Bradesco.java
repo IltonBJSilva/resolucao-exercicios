266dbc0298939f2b37e7f980228ae040e645e115
IltonBJSilva <xlsonplay@gmail.com>
2018-12-05 16:45:44 -0200

    Corrigi o comentario

--- Bradesco.java
+++ Bradesco.java
@@ -1,12 +1,13 @@
-public class Bradesco {
-    /*
-     * Nome do autor: Ilton Batista da Silva Júnior Data de criação do arquivo:
-     * 05/12/2018 Objetivo sucinto do programa: Programa para demonstração do uso de
-     * THIS (Programação Orientada a Objetos) Referência ao enunciado/origem do
-     * exercício: Exercício https://docs.google.com/forms/d/e/
-     * 1FAIpQLSckb5FSZXqMVsgf3gF831PDLcp0iZySindzJ1P4hkZq7wg0og/viewform Informações
-     * adicionais: https://goo.gl/MfFPuz
-     */
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior Data de criação do arquivo:
+ * 05/12/2018 Objetivo sucinto do programa: Programa para demonstração do uso de
+ * THIS (Programação Orientada a Objetos) Referência ao enunciado/origem do
+ * exercício: Exercício https://docs.google.com/forms/d/e/
+ * 1FAIpQLSckb5FSZXqMVsgf3gF831PDLcp0iZySindzJ1P4hkZq7wg0og/viewform Informações
+ * adicionais: https://goo.gl/MfFPuz
+ */
+public class Bradesco 
+{
     int dinheiro = 200;
     int banco = 600;
     int imposto = -4;
