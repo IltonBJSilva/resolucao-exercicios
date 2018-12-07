a7f3881fafb6ecf8bc2d2615cd9ab44f7f400118
IltonBJSilva <xlsonplay@gmail.com>
2018-12-07 16:00:19 -0200

    Cria classe Circle e TestaCircle

---
+++ Circle/Circulo.java
@@ -0,0 +1,60 @@
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior 
+ * Data de criação do arquivo: 06/12/2018 
+ * Objetivo sucinto do programa: Criar raio, cor, circuferencia e area do circulo
+ * Referência ao enunciado/origem do exercício:https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+ */
+ 
+ public class Circulo
+{ 
+    private double raio = 1.0;
+    private String cor = "red";
+
+    public Circulo(){
+
+    }
+    public Circulo(double raio){
+
+        this.raio = raio;
+    }
+    public Circulo(double raio, String cor) {
+
+        this.raio = raio;
+        this.cor = cor;
+    }
+
+    public double getRaio(){
+       
+        return this.raio;
+    }
+
+    public String getCor(){
+       return this.cor;
+    }
+
+    public void setRaio(double raio){
+    
+        this.raio = raio;
+
+    }
+
+    public void setCor(String cor){
+
+        this.cor = cor;
+
+    }
+
+    public String toString(){
+
+        return "Raio: " + raio + ", Cor: " + cor;
+
+    }
+
+    public double getArea(){
+        return Math.PI*(raio*raio);
+    }
+    public double getCircunferencia(){
+        
+        return 2 * Math.PI * raio;
+    }
+ }
---
+++ Circle/TestaCirculo.java
@@ -0,0 +1,42 @@
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior 
+ * Data de criação do arquivo: 06/12/2018 
+ * Objetivo sucinto do programa: Executar a classe TestaCirculo
+ * Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+ */
+
+public class TestaCirculo {
+    public static void main(String[] args) {
+        Circulo circuloUm = new Circulo();
+        Circulo circuloDois = new Circulo();
+        Circulo circuloTres = new Circulo();
+
+
+        
+        //Circulo1
+        System.out.println("CIRCULO 1:");
+        System.out.println("Cor = " + circuloUm.getCor());
+        System.out.println("Raio = " + circuloDois.getRaio());
+        System.out.println("Area = " + circuloUm.getArea());
+        System.out.println("Circuferencia = " + circuloUm.getCircunferencia());
+        System.out.println("---------------------------");
+
+        //Circulo2
+        System.out.println("CIRCULO 2:");
+        System.out.println("Cor = " + circuloTres.getCor());
+        System.out.println("Raio = " + circuloDois.getRaio());
+        System.out.println("Area = " + circuloDois.getArea());
+        System.out.println("Circuferencia = " + circuloDois.getCircunferencia());
+        System.out.println("---------------------------");
+        
+        //Circulo3
+        System.out.println("CIRCULO 3:");
+        System.out.println("Cor = " + circuloTres.getCor());
+        System.out.println("Raio = " + circuloTres.getRaio());
+        System.out.println("Area = " + circuloTres.getArea());
+        System.out.println("Circuferencia = " + circuloTres.getCircunferencia());
+        System.out.println("---------------------------");
+    
+    }
+
+}
