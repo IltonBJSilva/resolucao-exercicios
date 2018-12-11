8b28d0e648a1c61d79d552f05577a46aefd42f69
IltonBJSilva <xlsonplay@gmail.com>
2018-12-11 15:29:37 -0200

    Adicionar informação da fatura na tela

---
+++ Fatura/Fatura.java
@@ -0,0 +1,63 @@
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior 
+ * Data de criação do arquivo: 11/12/2018 
+ * Objetivo sucinto do programa: Mostar as informações relacionada a fatura como id, descrição, quantidade e o preço po unidade
+ * Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+ */
+
+public class Fatura 
+{
+    int id;
+    String descricao;
+    int quantidade;
+    double precoUnidade;
+
+    public Fatura(){}
+    
+    public Fatura(int id, String descricao, int quantidade, double precoUnidade ){
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.precoUnidade = precoUnidade;
+    }
+    //retorna ID
+    public int getId(){
+    return id;
+    }
+    
+    //retorna descrição
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    //retorna a quantidade
+    public int getQuantidade(){
+        return quantidade;
+    }
+    
+    // altera quantidade da classe 
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+    public double PrecoUnidade(){
+        return precoUnidade;
+    }
+
+    //altera preçoUnidade da classe
+    public void setPrecoUnidade(double precoUnidade){
+        this.precoUnidade = precoUnidade;
+    }
+
+    //mostra o total
+    public double getPrecoTotal(){
+        return (quantidade * precoUnidade);
+    }
+
+   public String toString(){
+        return "Fatura[Quantidade: " + quantidade + "\n preço por unidade: " + precoUnidade + "\n ID: " + id + "descrição: " + descricao + "]"; 
+    }
+
+
+
+}    
---
+++ Fatura/TestaFatura.java
@@ -0,0 +1,47 @@
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior 
+ * Data de criação do arquivo: 11/12/2018 
+ * Objetivo sucinto do programa: Testar a classe Fatura e iniciar a mesma mandando informações como paramentro
+ * Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1Uq6IUqO100w2ylIxVi17RTwYLo-ljPu2YGu7KPPDydg/edit
+ */
+
+public class TestaFatura {
+
+
+    public static void main(String[] args) {
+        //Faturas
+        Fatura fatura1 = new Fatura(1, "Primeira Fatura", 40, 20);
+        Fatura fatura2 = new Fatura(2, "Segunda Fatura",  20, 20);
+        Fatura fatura3 = new Fatura(3, "Terceira Fatura", 75, 20);
+
+        //Fatura 1
+        System.out.println("----------------------");
+        System.out.println("Fatura 1:");
+        System.out.println("ID: "+ fatura1.getId());
+        System.out.println("Preço por Unidade: " + fatura1.PrecoUnidade());
+        System.out.println("Quantidade: " + fatura1.getQuantidade());
+        System.out.println("Preço Total: " + fatura1.getPrecoTotal());
+        System.out.println("descrição: " + fatura1.getDescricao());
+        
+
+        //Fatura 2
+        System.out.println("----------------------");
+        System.out.println("Fatura 1:");
+        System.out.println("ID: " + fatura2.getId());
+        System.out.println("Preço por Unidade: " + fatura2.PrecoUnidade());
+        System.out.println("Quantidade: " + fatura2.getQuantidade());
+        System.out.println("Preço Total: " + fatura2.getPrecoTotal());
+        System.out.println("descrição: " + fatura2.getDescricao());
+
+        //Fatura 3
+        System.out.println("----------------------");
+        System.out.println("Fatura 1:");
+        System.out.println("ID: " + fatura3.getId());
+        System.out.println("Preço por Unidade: " + fatura3.PrecoUnidade());
+        System.out.println("Quantidade: " + fatura3.getQuantidade());
+        System.out.println("Preço Total: " + fatura3.getPrecoTotal());
+        System.out.println("descrição: " + fatura3.getDescricao());
+       
+    }
+
+}
