9629180d7ae4973f7292c9d47a56354beef763b1
IltonBJSilva <xlsonplay@gmail.com>
2018-12-21 14:49:20 -0200

    Demonstra oque e StringIndexOutOfBoundsException

---
+++ TratamentoDeExcecoes/StringIndexOutOfBoundsException/Demonstra_StringIndexOutOfBoundsException.java
@@ -0,0 +1,23 @@
+
+/*
+ * Nome do autor: Ilton Batista da Silva Júnior 
+ * Data de criação do arquivo: 21/12/2018 
+ * Objetivo sucinto do programa: 
+ * Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1cCI5x1i5kef9dWZKrvrobbZddDXuLhhK5Kw8Pdj-ouA/edit
+ */
+
+class Demonstra_StringIndexOutOfBoundsException{
+    public static void main(String[] args) {
+        try{
+            String nome = "Ilton";
+            char altera = nome.charAt(50);
+            System.out.println(altera);
+        } catch(StringIndexOutOfBoundsException e){
+            System.out.println("Não pode acessar essa parte na qual deseja");  
+        }
+     
+
+    }
+
+
+}
