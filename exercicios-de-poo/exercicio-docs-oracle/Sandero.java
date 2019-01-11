4d9754442b0bec5aa7dd5ddd22f6e12652abff09
IltonBJSilva <xlsonplay@gmail.com>
2019-01-11 10:10:25 -0200

    Altera data de criação do arquivo

--- exercicios-de-poo/exercicio-docs-oracle/Sandero.java
+++ exercicios-de-poo/exercicio-docs-oracle/Sandero.java
@@ -1,7 +1,7 @@
 class Sandero implements Car{
     /* 
  * Nome do autor: Ilton Batista da Silva Júnior 
- * Data de criação do arquivo: 09/01/2019 
+ * Data de criação do arquivo: 11/01/2019 
  * Objetivo sucinto do programa: Demonstar o uso na interface que pede que os metodos escritos sejam passado tbm para a class na qual foi implementada
  * Referência ao enunciado/origem do exercício:https://docs.oracle.com/javase/tutorial/java/concepts/QandE/questions.html
  */
@@ -13,13 +13,13 @@ class Sandero implements Car{
     String qualidade;
 
     //Mensagem de erro que gera quando não e chamado os metodos pedido pela interface
-    /**
-     * sers\ilton.silva\Desktop\TRABALHO\Resolucao\resolucao-exercicios\exercicios-de-poo\exercicio-docs-oracle\Sandero.java:1: error: Sandero is not abstract and does not override abstract method rodas(String) in Car
+    /*
+    sers\ilton.silva\Desktop\TRABALHO\Resolucao\resolucao-exercicios\exercicios-de-poo\exercicio-docs-oracle\Sandero.java:1: error: Sandero is not abstract and does not override abstract method rodas(String) in Car
     class Sandero implements Car{
     ^
     1 error
      */
-    
+
     public void aplicacaoFreios(int testa){
         this.testa = testa;
     }
