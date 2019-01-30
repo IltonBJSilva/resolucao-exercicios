3e031dc533c693153a9ad34f3ab2011347705db9
IltonBJSilva <xlsonplay@gmail.com>
2019-01-30 17:01:14 -0200

    Usa set e hash em busca for e fora

--- demonstra-collections/JavaCollectionMap.java
+++ demonstra-collections/JavaCollectionMap.java
@@ -10,21 +10,47 @@
 package br.com.sankhya.timtec.topicosespecial;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class JavaCollectionList {
+public class JavaCollectionMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Integer> myList = new ArrayList<>();
+		HashSet<String> mySet = new HashSet<>();
+		mySet.add("HakunaBatata");
+		mySet.add("BananaLisa");
 		
-		int numberOfElements = (int) (Math.random() * 100);
-		
-		for(int i = 0; i < numberOfElements; i++) {
-			myList.add(i);
+		System.out.println("\n________________________________________\nCom");
+		System.out.println("BananaLisa: " + mySet.contains("BananaLisa")); // True
+		System.out.println("HakunaBatata: " + mySet.contains("HakunaBatata")); // True
+		System.out.println("Copo: " + mySet.contains("copo")); //False
+		System.out.println("\n________________________________________\nSem");
+		mySet.remove("BananaLisa");
+		mySet.add("copo");
+		System.out.println("BananaLisa: " + mySet.contains("BananaLisa")); // false
+		System.out.println("copo: " + mySet.contains("copo")); //true
+		mySet.remove("copo");
+		System.out.println("\n________________________________________\n\n\n\n");
+
+		mySet.add("Banana");
+		mySet.add("Apple");
+		mySet.add("Orage");
+
+		for(String fruit : mySet) {
+			System.out.println(fruit);
 		}
-		for(int element : myList) {
-			System.out.println(element);
+		System.out.println("\n________________________________________\n\n\n\n");
+		
+		HashMap<String, Integer> myBooks = new HashMap <>();
+		myBooks.put("Ender's Game", 4);
+		myBooks.put("Harry Potter", 3);
+		myBooks.put("Books and Star Wars Word", 5);
+
+		for(String title : myBooks.keySet()) {
+			System.out.println(title);
+		}
+		for(int score: myBooks.values()) {
+			System.out.println(score);
 		}
 	}
-
 }
